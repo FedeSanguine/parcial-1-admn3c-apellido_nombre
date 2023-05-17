ad927423d070730b6bbda882dacb8c4c0e2c0e79
FedeSanguine <101578998+FedeSanguine@users.noreply.github.com>
2023-05-17 17:04:20 -0300

    creacion productos.js

---
+++ productos.js
@@ -0,0 +1,63 @@
+let productos = [
+    {
+        nombre: "Call of Duty Modern Warfare",
+        descripción: "...",
+        precio: 10000,
+        imagen: "",
+        imagen_descripción: "",
+        consola: 'Xbox One',
+        cant: 1,
+        id: 0
+    },
+    {
+        nombre: "GTA V",
+        descripción: "...",
+        precio: 10000,
+        imagen: "",
+        imagen_descripción: "",
+        consola: 'Xbox One',
+        cant: 1,
+        id: 1
+    },
+    {
+        nombre: "Battlefield V",
+        descripción: "...",
+        precio: 10000,
+        imagen: "",
+        imagen_descripción: "",
+        consola: 'Xbox One',
+        cant: 1,
+        id: 2
+    },
+    {
+        nombre: "Mortal Kombat 11",
+        descripción: "...",
+        precio: 10000,
+        imagen: "",
+        imagen_descripción: "",
+        consola: 'PS4',
+        cant: 1,
+        id: 3
+    },
+    {
+        nombre: "Fifa 22",
+        descripción: "...",
+        precio: 10000,
+        imagen: "",
+        imagen_descripción: "",
+        consola: 'PS4',
+        cant: 1,
+        id: 4
+    },
+    {
+        nombre: "Elden Ring",
+        descripción: "...",
+        precio: 10000,
+        imagen: "",
+        imagen_descripción: "",
+        consola: 'PS4',
+        cant: 1,
+        id: 5
+    },
+    
+]
